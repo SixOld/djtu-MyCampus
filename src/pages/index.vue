436a15e3ab2f84b3d7b80c32c91b28b8ffcdc39e
SixOld <37862970+SixOld@users.noreply.github.com>
2018-05-21 12:34:34 +0800

    页面美化

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,7 +2,7 @@
   @import "./src/less/config.less";
   page {
     background-image: url("https://raw.githubusercontent.com/SixOld/djtu-MyCampus-Six/master/img/background1.png");
-    background-position: 50% calc(~"100% + 380rpx");
+    background-position: 50% calc(~"100% rpx");
     background-repeat: no-repeat;
     background-size: 100%;
     background-attachment: fixed;
