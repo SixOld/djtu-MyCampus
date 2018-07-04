b042830f2c880365bb94df2d1ba7b9e929d3d836
SixOld <37862970+SixOld@users.noreply.github.com>
2018-07-04 08:42:40 +0800

    更改代取快递图标

--- src/util/index/index.js
+++ src/util/index/index.js
@@ -30,80 +30,10 @@ const funcs = [
   {
     name: '代取快递',
     url: 'express',
-    icon: '4',
+    icon: '5',
     bg: '#efefef',
-    type: 'login'
-  },
-/*{
-    name: '课程表',
-    url: 'schedule',
-    icon: 'kechengbiao',
-    bg: '#eacdd1',
-    type: 'login'
-  },
-  {
-    name: '成绩',
-    url: 'grade',
-    icon: 'chengji',
-    bg: '#99ccee',
-    type: 'login'
-  },
-  {
-    name: '自习',
-    url: 'classroom',
-    icon: 'building',
-    bg: '#90caaf',
     type: 'normal'
   },
-  {
-    name: '我的借阅',
-    url: 'library/loan',
-    icon: 'book',
-    bg: '#ab96c5',
-    type: 'login'
-  }, {
-    name: '历史借阅',
-    url: 'library/loan?type=history',
-    icon: 'book1',
-    bg: '#eea5d1',
-    type: 'login'
-  },
-  {
-    name: '图书搜索',
-    url: 'library/search',
-    icon: 'search',
-    bg: '#857e95',
-    type: 'normal'
-  },
-  {
-    name: '一卡通',
-    url: 'ecard',
-    icon: 'card',
-    bg: '#b92c18',
-    type: 'login'
-  },
-  {
-    name: '考表',
-    url: 'exam',
-    icon: 'exam',
-    bg: '#2578b5',
-    type: 'login'
-  },
-  {
-    name: '校历',
-    url: 'calendar',
-    icon: 'rili',
-    bg: '#008e59',
-    type: 'normal'
-  },
-  {
-    name: '校车',
-    url: 'bus',
-    icon: 'bus',
-    bg: '#ab96c5',
-    type: 'normal'
-  },
-*/
 ]
 
 module.exports = {
